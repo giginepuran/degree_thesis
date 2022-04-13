0e5a6684c03f25ad87793879de110d4ad43f6c3f
giginepuran <giginepuran@gmail.com>
2022-04-14 02:05:36 +0800

    complete function to return mode profile in LumAPI.py it will return a dict with keys "p_of_y" and "y"

--- lib/MyLumerical/LumAPI.py
+++ lib/MyLumerical/LumAPI.py
@@ -1,11 +1,6 @@
 import os
-import shutil
-import sys
 import lumapi
-from past.builtins import execfile
 import time
-from os.path import exists
-from lib.PSO import swarm
 
 
 def build_fsp(fdtd: lumapi.FDTD, script: str):
@@ -33,6 +28,30 @@ def get_fom(fdtd: lumapi.FDTD, lsf_script: str, fsp_path: str):
             fdtd.load(fsp_path)
             fdtd.eval(lsf_script)
             result = fdtd.getv('FOM')
+            fdtd.eval('newproject;')
+            time.sleep(5)
+            success = True
+        except:
+            count_f = count_f + 1
+            print(f'Getting FOM failed, count = {count_f}')
+            time.sleep(10)
+    return result
+
+
+def get_mode_profile_y(fdtd: lumapi.FDTD, fsp_path: str):
+    if not os.path.exists(fsp_path):
+        return -1
+    success = False
+    count_f = 0
+    lsf_script = open('E:/degree_thesis/script/lsf/P_wg2_far_profile.lsf', 'r').read()
+    result = {}
+    while not success:
+        try:
+            fdtd.eval('newproject;')
+            fdtd.load(fsp_path)
+            fdtd.eval(lsf_script)
+            result["p_of_y"] = fdtd.getv('PofY')
+            result["y"] = fdtd.getv('y')
             fdtd.eval('newproject;')
             time.sleep(5)
             success = True
